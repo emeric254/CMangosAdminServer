043f8fc0e5d9b9760c28dd1b45aa1ae68fda3513
emeric254 <emeric254@live.fr>
2016-06-01 22:27:53 +0200

    wip wrapper to communciate with CMaNGOS

--- code/core/SOAPTest.py
+++ code/core/SOAPTest.py
@@ -1,5 +1,17 @@
 
-# from zeep import Client
+import xmlrpc.client
+import zeep
+import zeep.wsse.username
 
-# client = Client('http://www.webservicex.net/ConvertSpeed.asmx?WSDL')
-# print(client.service.ConvertSpeed(100, 'kilometersPerhour', 'milesPerhour'))
+login = 'administrator'
+password = 'administrator'
+host = '10.0.0.125'
+port = '7878'
+path = 'urn:MaNGOS'
+
+# with xmlrpc.client.ServerProxy(uri='http://' + login + ':' + password + '@' + host + ':' + port + '/' + path + '/') as soapclient:
+#     print(soapclient)
+#     print(soapclient.system.listMethods())
+#     print(soapclient.executeCommand({'command': 'account onlinelist'}))
+
+# zeep.Client(wsdl='http://' + host + ':' + port + '/' + path, wsse=zeep.wsse.username.UsernameToken(login, password))
